4dbcf2b9b004e06489045d1001ad62606c4b965c
JuJaeng2 <jjy5230@gmail.com>
2023-05-13 23:19:27 +0900

    로그인 및 회원가입

--- src/main/java/Java_Caffe/java_caffe/controller/MemberController.java
+++ src/main/java/Java_Caffe/java_caffe/controller/MemberController.java
@@ -1,13 +1,54 @@
 package Java_Caffe.java_caffe.controller;
 
+import Java_Caffe.java_caffe.dto.UserDto;
+import Java_Caffe.java_caffe.service.SignUpService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
+
+//@RequestMapping("/Login") //mapping 앞부분을 통일시킴
 @Controller
+@RequiredArgsConstructor
 public class MemberController {
 
-    @GetMapping("/Login")
+    //생성자 주입(롭복의 RequiredArgsConstructor 사용)
+    public final SignUpService signUpService;
+
+
+
+    @GetMapping("/signUp")
+    public String signUp(){
+        return "parameterCheck";
+    }
+
+    // SignUp Controller
+    @PostMapping("/signUp")
+    public String dto(@ModelAttribute UserDto userDto){
+        System.out.println("Parameters =>"+userDto );
+        signUpService.save(userDto);
+        return "parameterCheck";
+    }
+
+    // Login Controller
+    @GetMapping("/login")
     public String login(){
+
         return "Login";
     }
+    @PostMapping("/login")
+    public String login(@ModelAttribute UserDto userDto, HttpSession session){
+        UserDto loginResult = signUpService.login(userDto);
+
+        if (loginResult != null){
+            //login 성공
+            session.setAttribute("loginId", loginResult.getUserId()); // session을 사용해서 로그인 유지
+            return "main";
+        }else {
+            //login 실패
+            return "Login";
+        }
+    }
+
 }
--- src/test/java/Java_Caffe/java_caffe/JavaCaffeApplicationTests.java
+++ src/test/java/Java_Caffe/java_caffe/JavaCaffeApplicationTests.java
@@ -1,13 +1,64 @@
-package Java_Caffe.java_caffe;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class JavaCaffeApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package Java_Caffe.java_caffe;
+//
+//import Java_Caffe.java_caffe.domain.UserEntity;
+//import Java_Caffe.java_caffe.repository.UserRepository;
+//import com.fasterxml.jackson.databind.annotation.JsonAppend;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//@SpringBootTest
+//class JavaCaffeApplicationTests {
+//
+//	@Autowired
+//	private UserRepository userRepository;
+//
+//	@Test
+//	void TestUser() {
+//
+//		User user1 = new User();
+////		user1.setU_id(1);
+//		user1.setU_name("테스트");
+//		user1.setU_pw("Test1234");
+//		this.userRepository.save(user1);
+//
+////		User user2 = new User();
+////		user1.setU_id(2);
+////		user2.setU_name("홍길동");
+////		user2.setU_pw("Test5678");
+////		this.userRepository.save(user2);
+//
+//	}
+//
+//	@Test
+//	void FindUser(){
+//
+//		List<User> all = this.userRepository.findAll();
+//		System.out.println("순수 데이터 : " + all);
+//		System.out.println("get()메서드 : "+all.get(0));
+//		System.out.println("테스트 => " + all.get(0).getU_name());
+//
+//
+//	}
+//
+//	@Test
+//	void FindById(){
+//
+//		Optional<User> userId = this.userRepository.findById(1);
+//		if (userId.isPresent()){
+//			System.out.println("순수 데이터 : "+userId);
+//			System.out.println("get()메서드 사용 : "+userId.get());
+//			System.out.println("get()메서드 + getU_name() : "+userId.get().getU_name());
+//		}
+//	}
+//
+//	@Test
+//	void FindByName(){
+//
+//		List<User> userName = this.userRepository.findByuserName("홍길동");
+//		System.out.println(userName.get(3).getU_id());
+//	}
+//}
